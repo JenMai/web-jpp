6c0b3e459b8b102066ac9a1fe51f50439cbc0b3f
JenMai <jm.jennimai@gmail.com>
2018-06-15 15:18:44 +0200

    ajout feature non finie

---
+++ TODO/validation.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<?php
+  echo "commande:";<br>
+  echo $_POST['firstname']," ",$_POST['name']," : email : ", $_POST['email'];
+  echo $_POST['nb_place'], " place adulte et ", $_POST['nb_place_e'], "place enfants."
+?>
+  <a href="https://twitter.com/intent/tweet/?url=&text=On va voir Jean Pierre Coffe"
+</body>
+</html>
